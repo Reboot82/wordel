873cec22ebe468da35323045be46cc308708c66c
Brandt Campbell <b82campbell@gmail.com>
2022-06-30 18:03:51 -0400

    words now have to be included in database

--- src/hooks/useWordel.js
+++ src/hooks/useWordel.js
@@ -1,138 +1,137 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const useWordel = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([...Array(6)]);
+  const [history, setHistory] = useState([]);
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys, setUsedKeys] = useState({});
 
-    const [turn, setTurn] = useState(0)
-    const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([...Array(6)])
-    const [history, setHistory] = useState([])
-    const [isCorrect, setIsCorrect] = useState(false)
-    const [usedKeys, setUsedKeys] = useState({})
+//   const [allSolutions, setAllSolutions] = useState([]);
 
-    // var allSolutions = [];
+  var howBoutThis = [];
 
-    
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        var solutionsArray = [];
+        for (let i = 0; i < json.length; i++) {
+          let singleSolution = json[i].word;
+          solutionsArray.push(singleSolution);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        howBoutThis = solutionsArray;
+      });
+  });
 
-    // useEffect(() => {
-    //     fetch("http://localhost:3001/solutions")
-    //       .then((res) => res.json())
-    //       .then((json) => {
-    //         for(let i = 0; i < json.length; i++){
-    //             const possibleSolution = json[i].word;
-    //             // addSolution(possibleSolution.word);
-    //             allSolutions.push = possibleSolution.word;
-    //             console.log(allSolutions[i], json[i].word);
-    //         }
-    //       });
-    //     //   console.log(allSolutions);
-    //   });
+  const formatGuess = () => {
+    let solutionArray = [...solution];
+    let formattedGuess = [...currentGuess].map((l) => {
+      return { key: l, color: "grey" };
+    });
 
-    const formatGuess = () => {
-        let solutionArray = [...solution]
-        let formattedGuess = [...currentGuess].map((l) => {
-            return {key: l, color: 'grey'}
-        })
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray[i] === l.key) {
+        formattedGuess[i].color = "green";
+        solutionArray[i] = null;
+      }
+    });
 
-        formattedGuess.forEach((l, i) => {
-            if (solutionArray[i] === l.key) {
-                formattedGuess[i].color = 'green'
-                solutionArray[i] = null
-            }
-        })
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray.includes(l.key) && l.color !== "green") {
+        formattedGuess[i].color = "yellow";
+        solutionArray[solutionArray.indexOf(l.key)] = null;
+      }
+    });
 
-        formattedGuess.forEach((l, i) => {
-            if (solutionArray.includes(l.key) && l.color !== 'green') {
-                formattedGuess[i].color = 'yellow'
-                solutionArray[solutionArray.indexOf(l.key)] = null
-            }
-        })
+    return formattedGuess;
+  };
 
-        return formattedGuess
+  const addNewGuess = (formattedGuess) => {
+    if (currentGuess === solution) {
+      setIsCorrect(true);
+    }
+    setGuesses((prevGuesses) => {
+      let newGuesses = [...prevGuesses];
+      newGuesses[turn] = formattedGuess;
+      return newGuesses;
+    });
+    setHistory((prevHistory) => {
+      return [...prevHistory, currentGuess];
+    });
+    setTurn((prevTurn) => {
+      return prevTurn + 1;
+    });
+    setUsedKeys((prevUsedKeys) => {
+      let newKeys = { ...prevUsedKeys };
 
+      formattedGuess.forEach((l) => {
+        const currentColor = newKeys[l.key];
+
+        if (l.color === "green") {
+          newKeys[l.key] = "green";
+          return;
+        }
+        if (l.color === "yellow" && currentColor !== "green") {
+          newKeys[l.key] = "yellow";
+          return;
+        }
+        if (
+          l.color === "grey" &&
+          currentColor !== "green" &&
+          currentColor !== "yellow"
+        ) {
+          newKeys[l.key] = "grey";
+          return;
+        }
+      });
+
+      return newKeys;
+    });
+    setCurrentGuess("");
+  };
+
+  const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      if (turn > 5) {
+        alert("you used all your guesses");
+        return;
+      }
+      if (history.includes(currentGuess)) {
+        alert("already tried it");
+        return;
+      }
+      if (currentGuess.length !== 5) {
+        alert("word must be 5 letters long");
+        return;
+      }
+      if (!howBoutThis.includes(currentGuess)) {
+        alert("word not in database");
+        return;
+      }
+      const formatted = formatGuess();
+      addNewGuess(formatted);
     }
 
-    const addNewGuess = (formattedGuess) => {
-
-        if (currentGuess === solution) {
-            setIsCorrect(true)
-        }
-        setGuesses((prevGuesses) => {
-            let newGuesses = [...prevGuesses]
-            newGuesses[turn] = formattedGuess
-            return newGuesses
-        })
-        setHistory((prevHistory) => {
-            return [...prevHistory, currentGuess]
-        })
-        setTurn((prevTurn) => {
-            return prevTurn + 1
-        })
-        setUsedKeys((prevUsedKeys) => {
-            let newKeys = {...prevUsedKeys}
-
-            formattedGuess.forEach((l) => {
-                const currentColor = newKeys[l.key]
-
-                if (l.color === 'green') {
-                    newKeys[l.key] = 'green'
-                    return
-                }
-                if (l.color === 'yellow' && currentColor !== 'green') {
-                    newKeys[l.key] = 'yellow'
-                    return
-                }
-                if (l.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow') {
-                    newKeys[l.key] = 'grey'
-                    return
-                }
-            })
-
-            return newKeys
-        })
-        setCurrentGuess('')
-
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
     }
 
-    const handleKeyup = ({ key }) => {
-
-        if(key === 'Enter'){
-            if(turn > 5){
-                alert('you used all your guesses')
-                return
-            }
-            if(history.includes(currentGuess)){
-                alert('already tried it')
-                return
-            }
-            if(currentGuess.length !== 5){
-                alert('word must be 5 letters long')
-                return
-            }
-            // for(allSolutions){
-
-            // }
-            const  formatted = formatGuess()
-            addNewGuess(formatted)
-        }
-
-        if(key === 'Backspace'){
-            setCurrentGuess((prev) => {
-                return prev.slice(0,-1)
-            })
-            return
-        }
-        
-        if (/^[A-Za-z]$/.test(key)) {
-            if (currentGuess.length < 5){
-                setCurrentGuess((prev) => {
-                    return prev + key
-                })
-            }
-        }
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        });
+      }
     }
+  };
 
-    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup}
+  return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup };
+};
 
-}
-
-export default useWordel
+export default useWordel;
